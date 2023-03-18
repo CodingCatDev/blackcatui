c62743e416cf400829c4224e51399f038eac1a22
Alex <alex@codingcat.dev>
2023-03-18 15:50:10 -0400

    Add AppHeader Initial

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,12 +8,13 @@
 
 	// Components & Utilities
 	import AppShell from '$lib/components/Layout/AppShell/AppShell.svelte';
+	import AppBar from '$lib/components/Layout/AppBar/AppBar.svelte';
 </script>
 
 <!-- App Shell -->
 <AppShell regionPage="overflow-y-scroll" slotFooter="bg-black p-4">
 	<!-- Header -->
-	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="header"><AppBar shadow="shadow-xl">Header</AppBar></svelte:fragment>
 
 	<!-- Sidebar (Left) -->
 	<svelte:fragment slot="sidebarLeft">Sidebar</svelte:fragment>
