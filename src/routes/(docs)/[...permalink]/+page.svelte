86398ffdd971b9d6cb09115f41bf80ff9fe0bf34
Alex <alex@codingcat.dev>
2023-03-23 10:40:47 -0400

    include missing page warning

--- src/routes/(docs)/[...permalink]/+page.svelte
+++ src/routes/(docs)/[...permalink]/+page.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
 	// Core
 	import DocsLayout from '../DocsLayout/DocsLayout.svelte';
+	import Alert from '$lib/components/Feedback/Alert/Alert.svelte';
+
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { ExclamationTriangle, XMark } from '@steeze-ui/heroicons';
 </script>
 
 <DocsLayout>
-	<svelte:fragment slot="bcu-docs-header" />
+	<svelte:fragment slot="bcu-docs-header">
+		<h1>Missing Implementation</h1>
+	</svelte:fragment>
 	<svelte:fragment slot="bcu-docs-detail">
-		<section class="flex flex-col gap-10">
-			<div class="flex flex-col gap-4">
-				<h1>Missing Implementation</h1>
-				<p>Please implement me.</p>
-			</div>
-		</section>
+		<Alert variant="variant-ghost-warning" slotIcon="w-8">
+			<!-- Icon -->
+			<svelte:fragment slot="bcu-alert-icon">
+				<Icon src={ExclamationTriangle} theme="solid" class="w-12" />
+			</svelte:fragment>
+
+			<!-- Message -->
+			<svelte:fragment slot="bcu-alert-message">
+				<h3 data-bcu-toc-ignore="">Alert</h3>
+				<p>
+					Please add this in the <span class="bcu-chip variant-ghost-primary">(docs)</span> route.
+				</p>
+			</svelte:fragment>
+		</Alert>
 	</svelte:fragment>
 </DocsLayout>
